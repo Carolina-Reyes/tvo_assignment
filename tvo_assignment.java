d890d4a0632d9182a6c1228477fd23e66b4d40b4
Carolina-Reyes <83788221+Carolina-Reyes@users.noreply.github.com>
2021-06-22 18:11:21 -0400

    Error learning

---
+++ tvo_assignment.java
@@ -0,0 +1,19 @@
+package com.tts;
+
+public class tvo_assignment {
+
+    public static void main(String []args){
+        int name= 1caro ;
+        boolean number =22,23 ;
+        char initial = c1 ;
+        int age= 22
+        long num= 25
+        int height
+        System.out.println(int height);
+        float 1time = 4;
+        
+
+
+
+    }
+}
