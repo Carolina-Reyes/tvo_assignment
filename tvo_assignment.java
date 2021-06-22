a89db0cb243a73c4cc0ea303af4e02f2a045c00c
Carolina-Reyes <83788221+Carolina-Reyes@users.noreply.github.com>
2021-06-22 18:16:49 -0400

    Added Errora

--- tvo_assignment.java
+++ tvo_assignment.java
@@ -1,17 +1,20 @@
 package com.tts;
 
 public class tvo_assignment {
+    String test;
 
     public static void main(String []args){
         int name= 1caro ;
         boolean number =22,23 ;
         char initial = c1 ;
-        int age= 22
-        long num= 25
-        int height
-        System.out.println(int height);
+        int age= 22;
+        long num= 25;
+        int height;
+        System.out.println( height);
+        System.out.println( test);
+
         float 1time = 4;
-        
+        String lname= 22;
 
 
 
